c2b0c49dfb8d4715ac4916544b801cb7f74a2bb0
Travis Hall <t.hall89d@gmail.com>
2021-07-28 12:13:05 -0400

    made orderBuilder obj in db to store custom orders and updated change event listeners for metals styles and sizes

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -1,29 +1,25 @@
-import { getSizes } from "./database.js"
+import { getSizes, setSize } from "./database.js";
 
-const sizes = getSizes()
+const sizes = getSizes();
 
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.name === "size") {
-            window.alert(``)
-        }
-    }
-)
+document.addEventListener("change", (event) => {
+  if (event.target.name === "size") {
+    setSize(parseInt(event.target.value));
+  }
+});
 
 export const DiamondSizes = () => {
-    let html = "<ul>"
+  let html = "<ul>";
 
-    // Use .map() for converting objects to <li> elements
-    const listItems = sizes.map(size => {
-        return `<li>
+  // Use .map() for converting objects to <li> elements
+  const listItems = sizes.map((size) => {
+    return `<li>
             <input type="radio" name="size" value="${size.id}" /> ${size.carets}
-        </li>`
-    })
+        </li>`;
+  });
 
-    html += listItems.join("")
-    html += "</ul>"
+  html += listItems.join("");
+  html += "</ul>";
 
-    return html
-}
-
+  return html;
+};
